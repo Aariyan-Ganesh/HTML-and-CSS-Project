e80d6f15b905d55cc5d6d7e841119824fb27060c
Aariyan Ganesh <78972296+Aariyan-Ganesh@users.noreply.github.com>
2023-01-21 20:10:33 +0000

    Create Basic_JavaScript_1.js
    Created the first function in JavaScript

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Hello, Aariyan! This is an alert notification..")
+
+function My_First_Function() 
+	{
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = 
+		result;
+	}
